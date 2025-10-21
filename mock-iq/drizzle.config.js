6a29e88ec0b848d6a0fa07598eea2fdc0c4471a3
jay1535 <habbujayanth@gmail.com>
2025-10-21 15:12:59 +0530

    Added db url

--- mock-iq/drizzle.config.js
+++ mock-iq/drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   schema: "./utils/schema.js",
   // out: "./drizzle",
   dbCredentials: {
-    url:process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+    url:'postgresql://mockInterview_owner:9EATRuF0vDUq@ep-icy-river-a5wy08h7-pooler.us-east-2.aws.neon.tech/mockInterview?sslmode=require&channel_binding=require',
   }
 });
